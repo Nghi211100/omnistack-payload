c230093aa70a08e0aee6bb2a8a3d6cb4fd3b5bd7
nghivo2111 <nghi.vo@omnistack.co>
2026-01-06 11:46:13 +0700

    chore: update project references from "Payload Website Template" to "Omnistack Website"

--- src/plugins/index.ts
+++ src/plugins/index.ts
@@ -15,7 +15,7 @@ import { getServerSideURL } from '@/utilities/getURL'
 import { vercelBlobStorage } from '@payloadcms/storage-vercel-blob'
 
 const generateTitle: GenerateTitle<Post | Page> = ({ doc }) => {
-  return doc?.title ? `${doc.title} | Payload Website Template` : 'Payload Website Template'
+  return doc?.title ? `${doc.title} | Omnistack Website` : 'Omnistack Website'
 }
 
 const generateURL: GenerateURL<Post | Page> = ({ doc }) => {
