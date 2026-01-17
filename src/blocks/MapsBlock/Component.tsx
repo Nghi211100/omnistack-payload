7c2ccd6d62eaaba8ca4c26d37e6f32ece96cac9d
nghivo2111 <nghi.vo@omnistack.co>
2026-01-17 19:17:16 +0700

    feat: update MapsBlock component layout to use flexbox for improved responsiveness; adjust iframe class for better height management

--- src/blocks/MapsBlock/Component.tsx
+++ src/blocks/MapsBlock/Component.tsx
@@ -17,14 +17,14 @@ const MapsBlock = (props: Props): React.JSX.Element => {
 
         <div
           className={cn(
-            'bg-white mx-auto w-full h-80 md:h-[700px] p-6 shadow-lg dark:bg-[#005eff]/20 dark:shadow-[inset_0px_-1px_1px_#132f4c] rounded-xl border border-neutral-100',
+            'bg-white flex flex-col w-full h-80 md:h-[700px] p-6 shadow-lg dark:bg-[#005eff]/20 dark:shadow-[inset_0px_-1px_1px_#132f4c] rounded-xl border border-neutral-100',
             { 'pt-0': props.specificAddress },
           )}
           style={{ width: `${props.settings?.width}px`, height: `${props.settings?.height}px` }}
         >
           <p className="text-center text-sm p-1">{props.specificAddress}</p>
           <iframe
-            className="border dark:border-[#1e4976] w-full h-[calc(100%-20px)]"
+            className="border dark:border-[#1e4976] w-full flex-1"
             loading="lazy"
             referrerPolicy="no-referrer-when-downgrade"
             src={`https://www.google.com/maps?q=${props?.latitude},${props?.longitude}&z=15&output=embed`}
